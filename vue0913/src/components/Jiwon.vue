3bdd500726832b5743b53a72c5f9fb0ae2a14d8f
jiweon21 <jna215843@gmail.com>
2021-09-14 15:26:15 +0900

    add:single file component

---
+++ vue0913/src/components/Jiwon.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <button @click="updateName">click</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "jiwon",
+    };
+  },
+  methods: {
+    updateName() {
+      this.name = "jiwon updated";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ vue0913/src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>This is Home page</h1>
+    <jiwon></jiwon>
+  </div>
+</template>
+
+<script>
+import Jiwon from "../components/Jiwon.vue";
+export default {
+  components: {
+    jiwon: Jiwon,
+  },
+  data() {
+    return {
+      name: "jiwon",
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
