5c129f2013cdf04fa90fa591deaff9cd6e76f416
jiweon21 <jna215843@gmail.com>
2021-09-23 16:49:33 +0900

    add: start vue-movie-app

---
+++ todo_alone/src/components/todoList.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+  <div>
+    <input type="checkbox" v-model="todo.done">
+      <span :class="{done: todo.done}">
+        {{todo.text}}
+      </span>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props:['todo']
+}
+</script>
+
+<style>
+
+</style>
