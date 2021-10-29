47ec9a86dddd03039ce9f558ab1005b667559040
jiweon21 <jna215843@gmail.com>
2021-10-29 17:27:03 +0900

    feat: add panel-tabs

---
+++ tab/src/components/InformationPanel.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <p class="panel-tabs">
+      <a :class="{'is-active': activeTab === 'Clinical'}" @click="activeTab = 'Clinical'">
+        Clinical
+      </a>
+      <a :class="{'is-active': activeTab === 'Images'}" @click="activeTab = 'Image'">
+        Image
+      </a>
+    </p>
+    <p class="panel-block">
+      <table class="table">
+        <tbody>
+          <tr>
+            <td>name</td>
+            <td>~~~</td>
+          </tr>
+          <tr>
+            <td>width</td>
+            <td>~~~</td>
+          </tr>
+          <tr>
+            <td>height</td>
+            <td>~~~</td>
+          </tr>
+          <tr>
+            <td>resolution</td>
+            <td>~~~</td>
+          </tr>
+          <tr>
+            <td>magnification</td>
+            <td>~~~</td>
+          </tr>
+        </tbody>
+      </table>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ tab/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      Navbar..
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
