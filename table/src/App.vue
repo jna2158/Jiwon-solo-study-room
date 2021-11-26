84a3734520329627ab4557284869851c10b8a884
jiweon21 <jna215843@gmail.com>
2021-11-26 15:26:47 +0900

    fix: Rename project

---
+++ table/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <div class="overflow-auto">
+      <b-table
+        id="my-table"
+        :items="items"
+        :per-page="perPage"
+        :current-page="currentPage"
+        small
+      >
+      </b-table>
+      <b-pagination
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+        aria-controls="my-table"
+      >
+      </b-pagination>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      // 몇개 씩 보여줄지
+      perPage: 3,
+      // 현재 페이지
+      currentPage: 1,
+      items: [
+        { id: 1, first_name: "Fred", last_name: "Flintstone" },
+        { id: 2, first_name: "Wilma", last_name: "Flintstone" },
+        { id: 3, first_name: "Barney", last_name: "Rubble" },
+        { id: 4, first_name: "Betty", last_name: "Rubble" },
+        { id: 5, first_name: "Pebbles", last_name: "Flintstone" },
+        { id: 6, first_name: "Bamm Bamm", last_name: "Rubble" },
+        { id: 7, first_name: "The Great", last_name: "Gazzoo" },
+        { id: 8, first_name: "Mr", last_name: "Slate" },
+        { id: 9, first_name: "Pearl", last_name: "Slaghoople" },
+      ],
+    };
+  },
+  computed: {
+    rows() {
+      return this.items.length;
+    },
+  },
+};
+</script>
+​
