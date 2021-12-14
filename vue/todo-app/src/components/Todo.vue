c1106fd02d6039638093d2ee14fd39b12fa940fa
jiweon21 <jna215843@gmail.com>
2021-12-14 10:53:31 +0900

    docs: change file directory

---
+++ vue/todo-app/src/components/Todo.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="mb-2">
+    <input type="checkbox" :checked="todo.checked" @change="toggleCheckbox" />
+    <span
+      class="ml-3"
+      :class="todo.checked ? 'text-muted' : ''"
+      :style="todo.checked ? 'text-decoration:line-through' : ''"
+      >{{ todo.text }}</span
+    >
+    <button class="btn btn-danger btn-sm" @click="clickDelete">Delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    toggleCheckbox(event) {
+      this.$emit("toggle-checked", {
+        id: this.todo.id,
+        checked: event.target.checked,
+      });
+    },
+    clickDelete() {
+      this.$emit("click-delete", this.todo.id);
+    },
+  },
+};
+</script>
+
+<style></style>
