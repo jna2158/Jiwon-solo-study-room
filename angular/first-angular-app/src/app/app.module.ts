6a09814d12a8d6a021395ace98734daadd9742d9
jna2158 <jna2158@naver.com>
2022-03-08 21:40:57 -0800

    feat: make 'servers' component and style

--- angular/first-angular-app/src/app/app.module.ts
+++ angular/first-angular-app/src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
