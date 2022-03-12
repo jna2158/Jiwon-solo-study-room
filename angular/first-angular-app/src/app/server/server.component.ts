5568477b66ec319455c10ee4c87a96775b7a38b3
jna2158 <jna2158@naver.com>
2022-03-12 13:06:01 -0800

    feat: event binding

--- angular/first-angular-app/src/app/server/server.component.ts
+++ angular/first-angular-app/src/app/server/server.component.ts
@@ -6,5 +6,10 @@ import { Component } from "@angular/core";
 })
 
 export class ServerComponent {
+  serverID: number = 10;
+  serverStatus: string = "offline";
 
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
--- angular/first-angular-app/src/app/servers/servers.component.ts
+++ angular/first-angular-app/src/app/servers/servers.component.ts
@@ -2,14 +2,23 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: `<app-server></app-server><app-server></app-server>`,
-  styleUrls: ['./servers.component.css']
+  //template: `<app-server></app-server><app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!!';
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  ngOnInit(): void {}
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!!';
+  }
 }
