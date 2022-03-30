857ee12c0ac8b52b75b18c04efbbf85cc61c158a
jna2158 <jna2158@naver.com>
2022-03-29 23:49:21 -0700

    feat: ngFor

--- angular/first-angular-app/src/app/server/server.component.ts
+++ angular/first-angular-app/src/app/server/server.component.ts
@@ -3,6 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
+  styles: [
+    `
+      .online {
+        color: white;
+      }
+    `,
+  ],
 })
 export class ServerComponent {
   serverID: number = 10;
--- angular/first-angular-app/src/app/servers/servers.component.ts
+++ angular/first-angular-app/src/app/servers/servers.component.ts
@@ -11,6 +11,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!!';
   serverName = ' ';
   serverCreated = false;
+  servers = ['TestServer', 'TestServer2'];
 
   constructor() {
     setTimeout(() => {
@@ -22,6 +23,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = `Server was created!! Name is ${this.serverName}`;
   }
 
