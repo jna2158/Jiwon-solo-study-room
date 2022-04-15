9db898ad84971002a7f5235b2e6b9fc5226489a8
jiweon21 <jna215843@gmail.com>
2022-04-15 10:50:22 +0900

    feat: Make SingleTodo component

--- react/first-react-typescript/src/components/SingleTodo.tsx
+++ react/first-react-typescript/src/components/SingleTodo.tsx
@@ -1,14 +1,42 @@
 import React from "react";
 import { Todo } from "../model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import "./style.css";
 
 type Props = {
+  key: number;
   todo: Todo;
-  todos: Todo[];
+  //todos: Todo[];
+  todos: any;
   setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
-  return <div></div>;
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  return (
+    <form className="todos_single">
+      <span className="todos_single--text">{todo.todo}</span>
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon">
+          <AiFillDelete />
+        </span>
+        <span className="icon">
+          <MdDone onClick={() => handleDone(todo.id)} />
+        </span>
+      </div>
+    </form>
+  );
 };
 
 export default SingleTodo;
